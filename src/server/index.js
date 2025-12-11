dfbe66ffad45a84fa04ee87dbad4a2d31514e9d1
Joaquin Briceno <joaquinbriceno1@gmail.com>
2025-12-10 20:16:21 -0600

    added ping endpoint for testing purposes

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,8 @@ const os = require('os');
 const app = express();
 
 app.use(express.static('dist'));
+
+app.post('/api/ping', (req, res) => res.send('pong'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
