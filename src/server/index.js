da1864755e31c67dba92fc66608e003ee7017ac2
Joaquin Briceno <joaquinbriceno1@gmail.com>
2025-12-15 18:18:03 -0600

    added cors

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const os = require('os');
+const cors = require('cors');
 
 const Utils = require('./libs/utils');
 
 const submittedPayments = [];
 const app = express();
 
+app.use(cors({ origin: '*' }));
 app.use(express.static('dist'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
